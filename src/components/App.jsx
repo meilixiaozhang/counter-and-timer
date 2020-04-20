01e46ca7623cc22de7b9a3e5ed47c9d12226c3a3
meilixiaozhang <redpangci@gmail.com>
2020-04-20 21:20:42 +1000

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+  /////////////////////////Real time clock //////////////////////////
+  setInterval(getTime, 1000);
+
+  const timeNow = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(timeNow);
+
+  function getTime() {
+    const timeThen = new Date().toLocaleTimeString();
+    setTime(timeThen);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <h1>{time}</h1>
+      <button className="timer" onClick={getTime}>
+        Get Time
+      </button>
+    </div>
+  );
+}
+
+export default App;
